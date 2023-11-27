5f768308e0c327feb87d87e09fcc4803e288b6e1
Akhmadaliyev <152167270+Akhmadaliyev@users.noreply.github.com>
2023-11-27 16:53:14 +0500

    uraaa muhammad jinni

---
+++ bin/blabla.dart
@@ -0,0 +1,5 @@
+import 'package:blabla/blabla.dart' as blabla;
+
+void main(List<String> arguments) {
+  print('Hello bvvvv world: ${blabla.calculate()}!');
+}
---
+++ test/blabla_test.dart
@@ -0,0 +1,8 @@
+import 'package:blabla/blabla.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
